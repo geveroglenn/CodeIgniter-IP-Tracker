71807daaa68446f0549e8673e69a3e7c9af7e31e
Glenn Gevero <ggevero@lancera.com>
2015-02-08 09:36:41 +0800

    User Agent Update
    Added User Agent tracking.

--- iptracker.php
+++ iptracker.php
@@ -13,10 +13,11 @@ class Iptracker{
 	private $sys = null;
 	
 	public function __construct(){
-		$this->sys	=& get_instance();			
+		$this->sys	=& get_instance();
+        $this->sys->load->library('user_agent');
 	}
 	
-	public static function get_ip_address(){		
+	private static function get_ip_address(){		
 		$ip = getenv('HTTP_CLIENT_IP')?:
 			getenv('HTTP_X_FORWARDED_FOR')?:
 			getenv('HTTP_X_FORWARDED')?:
@@ -26,23 +27,42 @@ class Iptracker{
 		return $ip;
 	}
 	
-	public static function get_page_visit(){
+	private function get_page_visit(){
 		return current_url();
 	}
+    
+    private function get_user_agent(){        
+        if ($this->sys->agent->is_browser()){
+            $agent = $this->sys->agent->browser().' '.$this->sys->agent->version();
+        }
+        elseif ($this->sys->agent->is_robot()){
+            $agent = $this->sys->agent->robot();
+        }
+        elseif ($this->sys->agent->is_mobile()){
+            $agent = $this->sys->agent->mobile();
+        }
+        else{
+            $agent = 'Unidentified User Agent';
+        }
+
+        return $agent;
+    }
 	
 	public function save_site_visit(){
 		$ip 	= self::get_ip_address();
-		$page	= self::get_page_visit();		
-		$seg = explode("-", $page);
+		$page	= self::get_page_visit();
+        $agent  = self::get_user_agent();
+		$seg    = explode("-", $page);
 		
-		if(!in_array('dimiyo', $seg)){			
+		if(!in_array('admin', $seg)){			
 			$data = array(
-				'ip'		=> $ip,
-				'page_view'	=> $page,
-				'date'		=> time()
+				'ip'            => $ip,
+				'page_view'     => $page,
+                'user_agent'    => $agent,
+				'date'          => time()
 			);
 			
-			$this->sys->db->insert('your_table_name', $data);			
+			$this->sys->db->insert('site_visits', $data);			
 		}
 	}
 }
